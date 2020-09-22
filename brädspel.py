35755328b8916651cd3f3e0613d2a9d59e0ef5a8
Drylum <josv2001@yh.nackademin.se>
2020-09-22 13:57:46 +0200

    test

--- brädspel.py
+++ brädspel.py
@@ -1,11 +1,33 @@
 import json
-
+"""
 with open('games.json') as f:
     data = json.load(f)
 
 for games in data['games']:
     print(games)
+"""
 #Fråga om man vill lägga till eller söka efter ett spel.
+a = (input("1: Vill du lägga till ett spel?\n 2: Se en lista över alla spel\n 3: Söka efter ett spel\n "))
+if a==1:
+    with open('games.json', 'w+') as w:
+        for name in games:
+            w.write(f'{name}\n')
+        for players in games:
+            w.write(f'{players}\n')
+        for time in games:
+            w.write(f'{time}\n')
+        for age in games:
+            w.write(f'{age}\n')
+elif a==2:
+    with open('games.json') as f:
+        for b in f.read():
+            print(b)
+
+elif a==3:
+    pass
+else:
+    pass
+
 
 
 #Om man vill lägga till ett sel ska de frågas efter Titel/antal spelare/tidsgång/rekomenderad ålder.
