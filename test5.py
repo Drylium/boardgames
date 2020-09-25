42f0ae015b30b28a939f35ea5610109edc19960b
Drylum <josv2001@yh.nackademin.se>
2020-09-25 09:39:00 +0200

    senaste

--- test5.py
+++ test5.py
@@ -1,6 +1,6 @@
 import json
 
-a = int(input("1.Lägga till ett spel\n2.Hitta ett spel\n3.Se lista över alla spel\n"))
+a = (input("1.Lägga till ett spel\n2.Hitta ett spel\n3.Se lista över alla spel\n"))
 while (a == 1):   #if you want to put in a game.
     data = {
         "games": {
@@ -14,21 +14,24 @@ while (a == 1):   #if you want to put in a game.
         json.dump(data, write_file, indent=4)
         json_string = json.dumps(data)
         
-    if input("Vill du lägga till fler?\n") != True:
-        continue
-    else:
-        print("Tack!")
-        break
+    answear = input("Vill du lägga till fler spel?\n")
+    while True:
+        if answear == "ja":
+            True
+            break
+        elif answear == "nej":
+            input("Tack!")
+            break
+        else:
+            answear = input("Svara med ett ja eller nej tack!\n")
+
 if (a == 2):    #if you want to withdraw.
-    print(name)
-    print(players)
-    print(time)
-    print(age)
+    pass
 elif (a == 3):    #if you want to see a list of all the games.
     with open("games.json") as f:
         for a in f.readlines():
             print(a)
 else:
-    print("Inte ett giltigt svar, försök igen.")
+    input("Inte ett giltigt svar, försök igen.")
 
 #put the dict's into json file.
