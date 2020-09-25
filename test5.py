6b3cdf485d4977f4529f42cdefeb0ad76a2d7889
Drylum <josv2001@yh.nackademin.se>
2020-09-25 14:54:31 +0200

    fortsätt

--- test5.py
+++ test5.py
@@ -1,8 +1,8 @@
 import json
 
-a = (input("1.Lägga till ett spel\n2.Hitta ett spel\n3.Se lista över alla spel\n"))
+a = int(input("1.Lägga till ett spel\n2.Hitta ett spel\n3.Se lista över alla spel\n4.Redigera ett spel\n5.Radera ett spel\n"))
 while (a == 1):   #if you want to put in a game.
-    data = {
+    data = {      
         "games": {
             "name": input("Namn: "),
             "players": input("Antal spelare: "),
@@ -10,20 +10,19 @@ while (a == 1):   #if you want to put in a game.
             "age": input("Rekomenderande ålder: ")
     }
 }
-    with open("games.json", "a+") as write_file:
+    with open("games.json", "a+") as write_file:    #put the dict's into json file.
         json.dump(data, write_file, indent=4)
         json_string = json.dumps(data)
         
-    answear = input("Vill du lägga till fler spel?\n")
-    while True:
-        if answear == "ja":
-            True
-            break
-        elif answear == "nej":
-            input("Tack!")
+    answer = input("Vill du lägga till fler spel?\n")    
+    while input != "ja":
+        if answer == "ja":
+            continue
+        elif answer == "nej":
+            print("Tack!")
             break
         else:
-            answear = input("Svara med ett ja eller nej tack!\n")
+            answer = input("Svara med ett ja eller nej tack!\n")
 
 if (a == 2):    #if you want to withdraw.
     pass
@@ -31,7 +30,16 @@ elif (a == 3):    #if you want to see a list of all the games.
     with open("games.json") as f:
         for a in f.readlines():
             print(a)
+elif (a == 4):
+    with open("games.json", "r+") as f:
+        data = json.load(f)
+        data["name"] = input()
+        f.seek(0)
+        json.dump(data, f, indent=4)
+        f.truncate()
+elif (a == 5):
+    pass
 else:
-    input("Inte ett giltigt svar, försök igen.")
+    input("Inte ett giltigt svar, försök igen.\n")
 
 #put the dict's into json file.
